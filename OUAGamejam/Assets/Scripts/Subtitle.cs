74c6a9167a6db3b1e8d6276c8e8736e20ef0c345
Bugra Kasarci <bugra.ksr@gmail.com>
2024-05-18 21:36:15 +0300

    Subtitle

---
+++ OUAGamejam/Assets/Scripts/Subtitle.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using UnityEngine;
+using TMPro;
+
+public class Subtitle : MonoBehaviour
+{
+    public TextMeshProUGUI subtitleText; 
+    public float displayDuration = 5f; 
+    public float typingSpeed = 0.05f; 
+
+    void Start()
+    {
+        subtitleText.gameObject.SetActive(false);
+    }
+
+    public void ShowSubtitle(string subtitle, float duration)
+    {
+        StartCoroutine(ShowSubtitleCoroutine(subtitle, duration));
+    }
+
+    private IEnumerator ShowSubtitleCoroutine(string subtitle, float duration)
+    {
+        subtitleText.gameObject.SetActive(true);
+        yield return StartCoroutine(TypeSubtitle(subtitle));
+
+        yield return new WaitForSeconds(duration);
+
+        subtitleText.gameObject.SetActive(false);
+    }
+
+    private IEnumerator TypeSubtitle(string subtitle)
+    {
+        subtitleText.text = "";
+        foreach (char letter in subtitle.ToCharArray())
+        {
+            subtitleText.text += letter;
+            yield return new WaitForSeconds(typingSpeed);
+        }
+    }
+}
---
+++ OUAGamejam/Assets/Scripts/Subtitle_Event.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Subtitle_Event : MonoBehaviour
+{
+    public Subtitle subtitleManager;
+    public GameObject canvas;
+    public string yazilacakYazi;
+
+    public void CallSubtitle()
+    {
+        canvas.SetActive(true);
+
+        subtitleManager.ShowSubtitle(yazilacakYazi, 5f);
+
+        StartCoroutine(HideCanvasAfterDuration(5f));
+    }
+
+    private IEnumerator HideCanvasAfterDuration(float duration)
+    {
+        yield return new WaitForSeconds(duration);
+        canvas.SetActive(false);
+    }
+}
