0645e825dea940b4f92d1f4f2960c05a5400bd90
Bugra Kasarci <bugra.ksr@gmail.com>
2024-05-20 20:19:03 +0300

    Portal

--- OUAGamejam/Assets/Scripts/NextLevel.cs
+++ OUAGamejam/Assets/Scripts/NextLevel.cs
@@ -6,7 +6,13 @@ using UnityEngine.SceneManagement;
 public class NextLevel : MonoBehaviour
 {
     public string nextLevel;
-
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            LoadNextLevel();
+        }
+    }
     public void LoadNextLevel()
     {
         SceneManager.LoadScene(nextLevel);
